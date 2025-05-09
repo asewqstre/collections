97e7d2d564e39921d5323bba289a9c3e8c4dd7e5
Aziz <aziz.mutallap.04az@gmail.com>
2025-05-09 17:55:29 +0600

    update_collection

--- collection/views.py
+++ collection/views.py
@@ -28,3 +28,20 @@ def delete_collection_view(request, pk):
     if request.method == 'POST': # Deleting the collection if form is submitted
         collection.delete()
     return render(request, 'collection/delete_collection.html', {'collection': collection})
+
+@login_required
+def update_collection_view(request, pk):
+    collection = Collection.objects.get(pk=pk)
+    if not Collection.objects.filter(user=request.user).exists(): # Checking if the collection belongs to the logged in user
+        # If the collection does not belong to the user, return a forbidden response
+        return HttpResponseForbidden('You are not allowed to update this collection.')
+    if request.method == 'POST':
+        form = Create_Collection_Form(request.POST, instance=collection)
+        if form.is_valid():
+            if request.user.collections.filter(name=collection.name).exclude(pk=pk).exists(): # Checking if the collection already exists in the logged in user's collections
+                return HttpResponseForbidden(f'You already have {collection.name} collection.')
+            collection.user = request.user # Resetting the user of collection to the logged in user because user field is empty while updating the collection
+            form.save()
+    else:
+        form = Create_Collection_Form(instance=collection)  # Pre-fill the form with the existing collection data
+    return render(request, 'collection/update_collection.html', {'form': form, 'collection': collection})
